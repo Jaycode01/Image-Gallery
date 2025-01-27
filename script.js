24efc5e7344741b5bf246f52b5de149ca70ef207
Joseph Lamidi <144225102+Jaycode01@users.noreply.github.com>
2025-01-27 23:44:52 +0100

    Update script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-// Toggle navigation menu on hamburger click
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
